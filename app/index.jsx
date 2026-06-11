b688d55ba04708ee1817cceb71ba672f7a12333c
Vedant4857 <vedantshekharb1@gmail.com>
2026-06-11 22:03:25 +0530

    Lec2

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,42 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import photo from "../assets/img/logo_icon.png";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Vedant4857</Text>
+    <View style={styles.container}>
+      <Image source={photo} style={styles.uestimg} />
+      <Text style={[styles.title, { color: "Blue" }]}>Uest School</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>School ERP</Text>
+
+      <View style={styles.card}>
+        <Text>Login</Text>
+      </View>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  uestimg: {
+    height: 80,
+    width: 80,
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
+  card: {
+    backgroundColor: "#eee",
+    padding: 20,
+    borderRadius: 5,
+    boxShadow: "4px 4px rgba(0,0,0,0.1)",
+  },
+});
