db8f273ba42ee395580be747ce6a76d1d0a15cc1
Vedant4857 <vedantshekharb1@gmail.com>
2026-06-11 21:33:59 +0530

    Lec1

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Vedant4857</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
